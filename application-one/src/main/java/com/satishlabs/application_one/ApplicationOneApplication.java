d2376f8671567583d5e80f67a0268c6bdaf99e49
satishlabs <spd61185@gmail.com>
2025-03-20 16:26:07 +0530

    Added spring boot applicaiton with docker

---
+++ application-one/src/main/java/com/satishlabs/application_one/ApplicationOneApplication.java
@@ -0,0 +1,13 @@
+package com.satishlabs.application_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationOneApplication.class, args);
+	}
+
+}
